acbe13e41278517b0db452ad8a1284d41012d742
Dan Abramov <dan.abramov@me.com>
2021-06-17 15:20:57 +0100

    Remove debouncing

--- src/components/Table/GlobalFilter.js
+++ src/components/Table/GlobalFilter.js
@@ -1,30 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Grid, Input, IconButton } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import ClearIcon from '@material-ui/icons/Clear';
 
-import useDebounce from '../../hooks/useDebounce';
 import { globalSearchStyles } from './tableStyles';
 
 function GlobalFilter({ onGlobalFilterChange }) {
   const [inputValue, setInputValue] = useState('');
-  const debouncedInputValue = useDebounce(inputValue, 300);
 
   const handleInputChange = e => {
     setInputValue(e.target.value);
+    onGlobalFilterChange(e.target.value);
   };
 
   const handleInputClean = () => {
     setInputValue('');
   };
-
-  useEffect(
-    () => {
-      onGlobalFilterChange(debouncedInputValue);
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [debouncedInputValue]
-  );
 
   const classes = globalSearchStyles();
 
